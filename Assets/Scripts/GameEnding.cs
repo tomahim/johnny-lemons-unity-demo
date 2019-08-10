0771bbc36d46126aadcf9aa6c5ecae1bc11df99a
tomabolt <thimblot@gmail.com>
2019-08-10 17:23:35 +0200

    init commit

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour {
+    public float fadeDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public float displayImageDuration = 10f;
+
+    bool m_IsPlayerCaught;
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter (Collider other) {  
+        if (other.gameObject == player) {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer () {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update() {
+        if (m_IsPlayerAtExit) {
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        } else if(m_IsPlayerCaught) {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart) {
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration) {
+            if (doRestart) {
+                SceneManager.LoadScene(0);
+            } else {
+                Application.Quit();
+            }
+        }
+    }
+}
